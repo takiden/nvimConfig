d45338ed400ba4e56e10fcecbca3e48ecfa950ae
Aref Takiden <takiden.a@gmail.com>
2026-08-17 23:12:25 +0200

    indentation

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,3 +10,19 @@ vim.o.autoread = true
 vim.o.laststatus = 3
 vim.o.cmdheight = 0
 vim.diagnostic.config({virtual_text=true})
+
+-- Global default: 2 spaces
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+
+-- Language-specific override for Go
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "go",
+--   callback = function()
+--     vim.opt_local.expandtab = false -- Go prefers standard tabs
+--     vim.opt_local.tabstop = 4
+--     vim.opt_local.shiftwidth = 4
+--   end,
+-- })
