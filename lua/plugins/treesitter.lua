26bb9cd056041e22dcb419ac579b53b706adf5b3
Aref Takiden <takiden.a@gmail.com>
2026-08-18 06:56:23 +0200

    auto_install option

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@ vim.pack.add({
 
 -- treesitter
   require("nvim-treesitter.config").setup({
- 				build=":TSUpdate",
- 				 install_dir = vim.fn.stdpath('data') .. '/site'
+    build=":TSUpdate",
+    install_dir = vim.fn.stdpath('data') .. '/site',
+    auto_install  = true
  })
  require("nvim-treesitter").install({"javascript", "typescript", "go", "gomod"})
