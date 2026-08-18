e92c682fc5094d3351f3492976df82c994353488
Aref Takiden <takiden.a@gmail.com>
2026-08-18 06:29:50 +0200

    clean up

--- init.lua
+++ init.lua
@@ -3,8 +3,8 @@ require("core.globalkeymaps")
 require("core.autocommands")
 require("core.formatting")
 require("core.colorscheme")
--- require("plugins.plugins")
--- In init.lua
+
+-- load plugins
 local plugins_path = vim.fn.stdpath("config") .. "/lua/plugins"
 for _, file in ipairs(vim.fn.readdir(plugins_path)) do
   if file:match("%.lua$") and file ~= "init.lua" then
